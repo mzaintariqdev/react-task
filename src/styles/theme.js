ee1b806c1e815bb398147611a7dcf1db9c675fe6
mzaintariqdev <mzaintariqdev@gmail.com>
2022-01-06 23:01:28 +0500

    first commit

---
+++ src/styles/theme.js
@@ -0,0 +1,43 @@
+const theme = {
+  colors: {
+    white: '#FFFFFF',
+    primary: '#2A7BE4',
+    secondary: '#748194',
+    success: '#00D27B',
+    skyBlue: '#27BCFE',
+    warning: '#F68F57',
+    danger: '#E63756',
+    light: '#F9FAFE',
+    dark: '#0B1828',
+    gray900: '#344050',
+    gray800: '#4D5969',
+    gray700: '#5D6E82',
+    gray600: '#748194',
+    gray500: '#9DA8BA',
+    gray400: '#B6C2D2',
+    gray300: '#D8E2EE',
+    gray200: '#EDF2F8',
+    gray100: '#F9FAFD',
+  },
+  font: {
+    family: 'Poppins, sans-serif',
+    weight: {
+      thin: 100,
+      extraLight: 200,
+      light: 300,
+      regular: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 700,
+      extraBold: 800,
+    },
+  },
+  media: {
+    md: 768,
+    sm: 500,
+    lg: 968,
+    xl: 1368,
+  },
+};
+
+export default theme;
