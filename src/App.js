77f000bf6fa3be106537a0a530e1ea5ec48d477d
mzaintariqdev <mzaintariqdev@gmail.com>
2022-01-08 04:38:35 +0500

    feat:navbar structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
- 
+import Header from "./components/header/header";
+import "./App.css";
+import { ThemeProvider } from "styled-components";
+
+import theme from "./styles/Theme";
+
 function App() {
   return (
-     <></>
+    <ThemeProvider theme={theme}>
+      <Header />
+    </ThemeProvider>
   );
 }
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import styled from "styled-components";
+import headerCart from "./headerCart/headerCart";
+import headerCurrency from "./headerCurrency";
 
-export default class header extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+export default class Header extends Component {
+  render() {
+    return (
+      <HeaderWrapper>
+        <HeaderLeft>
+          <HeaderLink></HeaderLink>
+          <HeaderLink></HeaderLink>
+          <HeaderLink></HeaderLink>
+        </HeaderLeft>
+        <HeaderCenter>
+          <HeaderIcon></HeaderIcon>
+        </HeaderCenter>
+        <HeaderRight>
+          <headerCurrency />
+          <headerCart />
+        </HeaderRight>
+      </HeaderWrapper>
+    );
+  }
 }
+const HeaderWrapper = styled.div`
+  min-width: 100vw;
+  background: ${(p) => p.theme.colors.cWhite};
+  font-family: "Raleway", sans-serif;
+  margin: 0;
+  height: 80px;
+`;
+
+const HeaderLeft = styled.div``;
+const HeaderLink = styled.div``;
+const HeaderCenter = styled.div``;
+const HeaderRight = styled.div``;
+const HeaderIcon = styled.div``;
